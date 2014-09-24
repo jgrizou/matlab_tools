f7ce2fafc5c041eeeaa1c85051583efbb46273d6
jgrizou <jonathan.grizou@gmail.com>
2014-09-24 16:42:17 -0500

    typo

--- system/files/cell2file.m
+++ system/files/cell2file.m
@@ -1,8 +1,8 @@
-function cell2file(finaleName, stringCell)
+function cell2file(fileName, stringCell)
 %CELL2FILE
 % this function take a cell of string and parse if into a file. Each cell element is one ligne
 
-fid = fopen(finaleName, 'w');
+fid = fopen(fileName, 'w');
 for i = 1:length(stringCell)
     fprintf(fid, stringCell{i});
     fprintf(fid, '\n');
